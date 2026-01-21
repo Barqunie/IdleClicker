2553f83c129b20f32644593a5267ac11eed22753
Barış Ayık <bybarqunie@gmail.com>
2026-01-21 20:14:11 +0300

    0

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -6,7 +6,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
+
 
 
 public class Clicker : MonoBehaviour
